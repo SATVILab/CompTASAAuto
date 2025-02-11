d56f9366eb44f096653ffedccfd207ccac6016e6
MunyaMusvosvi <99468900+MunyaMusvosvi@users.noreply.github.com>
2025-02-11 13:04:18 +0200

    Update run_analysis.R
    I have added the function to run the tasa analysis

--- scripts/run_analysis.R
+++ scripts/run_analysis.R
@@ -1,2 +1,4 @@
 # add R code to run analysis
 # path to data given by Sys.getenv("PATH_RAW_DATA")
+# path to store the results, could be a temporary folder that can be downloaded via a link by Sys.getenv("PATH_RESULT_FOLDER")
+gate_satvi_tasa(fcs_location = PATH_RAW_DATA ,results_location = PATH_RESULT_FOLDER)
